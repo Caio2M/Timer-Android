b1ef0d971cc99e4a46046bfc2dfa1ed9a79214c5
Caio2M <caiomarinhomelo05@gmail.com>
2023-11-30 17:25:14 -0300

    Added icons, list of saved times and styled it

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.example.timer
 
+import android.graphics.drawable.Icon
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.timer.ui.theme.TimerTheme
 import kotlinx.coroutines.delay
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +57,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class ListValue(val id: String, val text: String)
 @Composable
 fun App() {
     var hour by remember { mutableStateOf(0) }
@@ -45,6 +65,8 @@ fun App() {
     var second by remember { mutableStateOf(0) }
     var mili by remember { mutableStateOf(0) }
     var isPaused by remember { mutableStateOf(true) }
+
+    var listSavedTimes by remember { mutableStateOf(emptyList<ListValue>()) }
 
     LaunchedEffect(key1 = isPaused, key2 = mili, block = {
         if (!isPaused) {
@@ -68,35 +90,94 @@ fun App() {
     fun onStop () {
         mili = 0; second = 0; minute = 0; hour = 0
         isPaused = true
+        listSavedTimes = listSavedTimes.filter { it.text == "" }
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.align(Alignment.Center)){
+    fun onMark (text: String) {
+        listSavedTimes += ListValue(id = "${UUID.randomUUID()}", text)
+    }
+
+    fun formatNumber (num: Int): String {
+        if ("$num".length <= 1) {
+            return "0$num"
+        }
+        return "$num"
+    }
+
+    fun formatTime (hour: Int, min: Int, sec: Int, mil: Int): String {
+        return "${formatNumber(hour)}:${formatNumber(min)}:${formatNumber(sec)}:${formatNumber(mil)}"
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(20.dp)) {
+        Box(modifier = Modifier
+            .align(Alignment.TopCenter)
+            .offset(y = (100).dp)){
             Text(
                 fontWeight = FontWeight.Bold,
-                fontSize = 30.sp,
-                text =
-                    "${if ("$hour".length <= 1) "0$hour" else hour}:" +
-                    "${if ("$minute".length <= 1) "0$minute" else minute}:" +
-                    "${if ("$second".length <= 1) "0$second" else second}:" +
-                    "${if ("$mili".length <= 1) "0$mili" else mili}"
+                fontSize = 40.sp,
+                text = formatTime(hour, minute, second, mili)
             )
         }
         Row(modifier = Modifier
-            .align(Alignment.Center)
-            .offset(y = (100).dp),
+            .fillMaxWidth()
+            .align(Alignment.TopCenter)
+            .offset(y = (200).dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Button(
-                onClick = { isPaused = !isPaused }
-            ) {
-                Text(text = "${if (isPaused) "Play" else "Pause"}")
-            }
-            Box(modifier = Modifier.width(20.dp))
             Button(
                 onClick = { onStop() }
             ) {
-                Text(text = "Stop")
+                Icon(imageVector = ImageVector.vectorResource(id = R.drawable.stop), contentDescription = null)
             }
+            Button(
+                onClick = { isPaused = !isPaused }
+            ) {
+                if (isPaused) {
+                    Icon(Icons.Default.PlayArrow, contentDescription = null)
+                } else {
+                    Icon(imageVector =  ImageVector.vectorResource(id = R.drawable.pause), contentDescription = null)
+                }
+            }
+            Button(
+                onClick = {
+                    if (formatTime(hour, minute, second, mili).replace(":" ,"").toInt() > 0) {
+                        onMark(formatTime(hour, minute, second, mili))
+                    }
+                }
+            ) {
+                Icon(Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+        Box (modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.6f)
+            .align(Alignment.BottomCenter)
+        ) {
+            LazyColumn(
+                content = {
+                    items(listSavedTimes) {
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(0.dp, 5.dp)
+                        ){
+                            Text(text = it.text, fontSize = 20.sp)
+                            Text(
+                                text = formatNumber(listSavedTimes.indexOf(it) + 1),
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
+                })
+        }
+        if (listSavedTimes.isEmpty()) {
+            Text(color = Color.DarkGray, fontSize = 20.sp, text = "No time saved...", modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = 100.dp))
         }
     }
 }
